321a0d3dc2aa12db9e0da7983fd05a1b82778953
Alfonso Villalobos <alfonsocvu@gmail.com>
2024-03-29 18:13:25 -0600

    start serialization

--- src/monitor_brightness.rs
+++ src/monitor_brightness.rs
@@ -1,20 +1,26 @@
-use std::process::Command;
+use std::{fs, path::Path, process::Command};
+use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize)]
 struct Monitor {
     device_id: String,
     brightness_bin_location: String,
 }
 
+#[derive(Serialize, Deserialize)]
 pub(crate) struct MonitorBrightness {
     monitors: Vec<Monitor>,
 }
 
 impl MonitorBrightness {
-    
+    const FILE_NAME: &'static str = "monitors.cbor";
+
     pub fn new() -> MonitorBrightness {
-        let monitor_brightness = MonitorBrightness {
-            monitors: Vec::new(),
-        };
+        let (monitor_brightness, file_exists) = Self::load_from_file();
+
+        if file_exists {
+            return monitor_brightness;
+        }
 
         let ddccontrol_desc  = Command::new("ddccontrol")
                 .arg("-p")
@@ -39,5 +45,26 @@ impl MonitorBrightness {
         println!("{:?}", devices);
 
         return Vec::new();
+    }
+
+    fn load_from_file() -> (MonitorBrightness, bool) {
+        let default = MonitorBrightness {
+            monitors: Vec::new(),
+        }; 
+
+        if !Path::new("monitors.cbor").exists() {
+            return (default, false);
+        }
+        
+        let class_cbor_bytes = fs::File::open(Self::FILE_NAME).expect("Failed to open file");
+        let monitor_brightness: MonitorBrightness = serde_cbor::from_reader(class_cbor_bytes);
+
+        return (monitor_brightness, true);
+    }
+
+    fn save_to_file(&self) {
+        let class_file = File::create(Self::FILE_NAME).expect("Failed to create file");
+
+        let class_postcard_bytes = serde_cbor::to_writer(class_file, &self);
     }
 }
